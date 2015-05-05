b195a5d4d5f7447a9e316f193dbbfb509b01ad1e
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2015-05-05 11:59:20 +0200

    Mise à jour pour éviter la contrainte du non flush de la console. http://stackoverflow.com/questions/29528410/strange-behaviour-of-console-log-on-firefox-37

--- javascript-2-callback/exos/exo1/solution/toto.js
+++ javascript-2-callback/exos/exo1/solution/toto.js
@@ -1,10 +1,14 @@
+window.onload = function() {
+var res = document.getElementById("cpt");
+
 function call1 (i) {
   if (i < 200000) {
     i++;
-    console.log("i -->" + i);
+    console.log(i);
+    res.innerHTML = i
     setTimeout(call1, 0, i);
   }
 }
 
 setTimeout(call1, 0, 0);
-
+}
