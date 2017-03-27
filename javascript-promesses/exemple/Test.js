29b49c179b6cf440e080a561ad4db5d9b0a3e2e5
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2017-03-27 10:45:11 +0200

    Completion du test

--- javascript-promesses/exemple/Test.js
+++ javascript-promesses/exemple/Test.js
@@ -1,8 +1,14 @@
+Promise = require('bluebird');
+
 var rp = require('request-promise');
 
 rp('http://www.insa-lyon.fr')
   .then(function (htmlString) {
-  	console.log('-->', htmlString);
+  	//console.log('-->', htmlString);
     res = htmlString.match(/https{0,1}:\/\/([^'"]*)["']/g);
-    console.log("===>", res);
+    //console.log("===>", res);
+    Promise.map(res, function(elem) {
+      //console.log("-->", elem.substring(0, elem.length-1));
+      rp(elem.substring(0, elem.length-1)).then(function(resHtml) { console.log("==>", resHtml); })
+    })
   })
