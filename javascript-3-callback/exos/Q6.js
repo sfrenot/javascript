d570f6e6016fa6c170a2985afdbd9a20c9c8b01b
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2018-03-07 18:05:30 +0100

    Ajout flow

--- javascript-3-callback/exos/Q6.js
+++ javascript-3-callback/exos/Q6.js
@@ -1,3 +1,23 @@
 fs = require('fs');
 
-fs.readFile('/tmp/toto.txt', 'utf8', function(err, data) { console.log("->", data)})
+http = require('http');
+fs.readFile('/tmp/toto.txt', 'utf8',
+  function(err, data) {
+     http.get(data, function(res) {
+       res.setEncoding('utf8');
+       res.on('data', function(chunk) {
+         console.log(chunk);
+       });
+     });
+  }
+);
+
+//
+// request = require('request');
+// fs.readFile('/tmp/toto.txt', 'utf8',
+//   function(err, data) {
+//      request(data, function(err, res) {
+//        console.log(res);
+//      });
+//   }
+// );
