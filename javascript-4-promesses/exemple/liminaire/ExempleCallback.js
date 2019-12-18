a46b45a896a91b7f729caa5e53046482a7f61206
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2019-12-18 16:30:28 +0100

    Suppression traces

--- javascript-4-promesses/exemple/liminaire/ExempleCallback.js
+++ javascript-4-promesses/exemple/liminaire/ExempleCallback.js
@@ -10,9 +10,7 @@ fs.readFile('./web.txt', 'utf8', function(err, res) {
   request.get(res, function(err, res) {
     pages = res.body.match(/https{0,1}:\/\/([^'"]*)/g);
     pages.forEach(function (elem) {
-      // console.log('->', elem.substring(0,elem.length-1));
       request.get(elem.substring(0,elem.length-1), function(err, res) {
-        // console.log('-->', res.body);
         if (res) {
           pages = res.body.match(/https{0,1}:\/\/([^'"]*)["']/g);
           if (pages) {
