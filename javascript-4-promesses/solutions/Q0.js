2eaaa28472071bec5ae5aa1e65adf56f37281f68
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2020-12-17 15:15:00 +0100

    Correction d'une solution

--- javascript-4-promesses/solutions/Q0.js
+++ javascript-4-promesses/solutions/Q0.js
@@ -1,8 +1,11 @@
-fs = require('fs-extra-promise');
+require('fs-extra-promise');
+.readFileAsync('./toto.txt', 'utf-8')
+.then( (res) => { console.log(res); })
 
-fs.readFileAsync('./toto.txt', 'utf-8')
-.then( 
-  (res) => {
-    console.log(res);
-  }
-)
+//
+
+require('fs');
+.readFile('./toto.txt', 'utf8', function(err, res) {
+  console.log(res);
+})
+
