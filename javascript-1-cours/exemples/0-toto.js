77238cfe08f8f6fbd526aabf455f28641e321e0a
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2021-12-03 12:03:23 +0100

    Ajout toto

--- javascript-1-cours/exemples/0-toto.js
+++ javascript-1-cours/exemples/0-toto.js
@@ -1,2 +1,9 @@
-i = 2
-console.log("coucou" + i)
+function create() {
+  reponse = 23;
+  return function (x) { return x + reponse; }
+}
+
+reponse = 100
+let a = create()
+console.log(a(12))
+console.log(reponse)
