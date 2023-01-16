e6cd4497540c289fcb890900c06d737b1fd4d99b
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2023-01-16 12:29:27 +0100

    Modification code de test

--- javascript-4-promesses/exemple/Test.js
+++ javascript-4-promesses/exemple/Test.js
@@ -1,14 +1,22 @@
-Promise = require('bluebird');
-
+//Promise = require('bluebird');
 var rp = require('request-promise');
 
 rp('http://www.insa-lyon.fr')
-  .then(function (htmlString) {
-  	//console.log('-->', htmlString);
-    res = htmlString.match(/https{0,1}:\/\/([^'"]*)["']/g);
-    //console.log("===>", res);
-    Promise.map(res, function(elem) {
-      //console.log("-->", elem.substring(0, elem.length-1));
-      rp(elem.substring(0, elem.length-1)).then(function(resHtml) { console.log("==>", resHtml); })
-    })
+.then(function (htmlString) {
+  res = htmlString.match(/https{0,1}:\/\/([^'"]*)["']/g);
+  res.forEach( function(elem) {
+    console.log("->", elem);
   })
+
+  /*
+  //console.log("===>", res);
+  Promise.map(res, function(elem) {
+    console.log("-->", elem.substring(0, elem.length-1));
+    rp(elem.substring(0, elem.length-1))
+    .then(function(resHtml) {
+      console.log("==>", resHtml.match(/https{0,1}:\/\/([^'"]*)["']/g));
+    })
+    .catch(function(err) {})  
+  })
+   */
+})
